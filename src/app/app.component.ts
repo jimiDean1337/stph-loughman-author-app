39224f5f749c269ba5200e5673068c10f033023f
jimi flynn <flynn-3199@cotc.edu>
2021-04-13 12:32:40 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sla-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sla-web-app';
+}
